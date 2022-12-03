ed25100dd8732d55c7a1f73294b5cd55347f6717
GuilhermeRB1 <105435063+GuilhermeRB1@users.noreply.github.com>
2022-12-03 13:44:45 -0300

    Add files via upload

---
+++ agendamysql/config/agenda.sql
@@ -0,0 +1,51 @@
+-- Processo de criação do banco de dados e da tabela para utilização do sistema.
+
+-- Configurações MySQL.
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Criando o banco de dados.
+
+CREATE DATABASE Agenda;
+
+-- Usando o banco de dados.
+
+USE Agenda;
+
+-- Criando a a tabela.
+
+CREATE TABLE  Pessoa (
+id INT unsigned PRIMARY KEY auto_increment COMMENT 'Identificador (PK)',
+nome VARCHAR(255) NOT NULL COMMENT 'Nome da pessoa',
+sexo CHAR(1) NOT NULL COMMENT 'Sexo da pessoa (Masculino, Feminino, Não informado)',
+data_nascimento DATE NOT NULL COMMENT 'Data de nascimento da pessoa',
+telefone decimal (13,0) NOT NULL COMMENT 'Telefone com DDD e codigo do pais (apenas digitos)',
+email VARCHAR (255) NOT NULL COMMENT 'Email da pessoa',
+senha VARCHAR(255) NOT NULL COMMENT 'Senha da pessoa'
+);
+
+
+-- Criptografando o valor da senha no banco de dados.
+
+UPDATE Pessoa
+SET senha=SHA1('123456');
+
+-- Criando o usuário da aplicação.
+
+    -- MySQL Workbench:
+
+CREATE USER 'agenda_contatos'@'localhost' IDENTIFIED WITH mysql_native_password BY 'agenda123'; 
+
+    -- MySQL MariaDB:
+
+CREATE USER 'agenda_contatos'@'localhost' IDENTIFIED BY 'agenda123'; 
+
+-- Definindo permissões para o usuário da aplicação.
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON Agenda.* TO 'agenda_contatos'@'localhost';
+
+-- Criando o usuário root para acessar a agenda.
+
+INSERT INTO Pessoa (nome,sexo,data_nascimento,telefone,email,senha) VALUES ('root','N','2000-01-01','00000','root@teste.com','cf2e875d70c402e4aaf32ceb64b1fa6f7396af59');
---
+++ agendamysql/config/conexao.php
@@ -0,0 +1,19 @@
+<?php
+
+# Configuração de conexão com MySQL.
+
+$database = "Agenda";
+$host = 'localhost';
+$username = 'agenda_contatos';
+$password = 'agenda123';
+
+# Efetuando a conexão e tratando possíveis erros.
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$database;", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $erro) {
+    echo "Erro: " . $erro->getMessage();
+} catch (Exception $err) {
+    echo "Erro: " . $err->getMessage();
+}
